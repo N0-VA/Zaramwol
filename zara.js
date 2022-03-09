8aacf65d8ad724c6879c2fd5a5df5beb1eb7e047
Nova <98613828+N0-VA@users.noreply.github.com>
2022-03-09 13:39:03 +0100

    Update zara.js

--- zara.js
+++ zara.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+2347042930688' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'FY NOVA 2k' : process.env.OWNER_NAME,
 
 };
